f5579882602ffb456bd6458a4f6bf7a18f0ebecc
consolexyz <mosesobike@gmail.com>
2023-06-13 12:21:42 +0100

    Revert "add react router"
    This reverts commit 89e7751c70bf07684b66874a94c821701ab9a23f.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/homepage/HomePage";
-import About from "./pages/Aboutpage/About";
-import Works from "./pages/workspage/Works";
-import Contact from "./pages/contactpage/Contact";
-
-
 import "./App.scss"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes >
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path= "/works" element={<Works />} />
-      <Route path= "/contact" element={<Contact/>} />
-      </ Routes>
+      <HomePage />
     </div>
   );
 }
