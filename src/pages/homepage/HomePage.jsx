453391c3a632dcb7dc76166e999a9a71328a864c
consolexyz <mosesobike@gmail.com>
2023-07-09 13:07:02 +0100

    new css change

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './homepage.scss';
 import { gsap } from 'gsap';
 import { ExpoScaleEase ,} from "gsap/EasePack";
@@ -8,33 +8,31 @@ const  HomePage= () =>{
 
   gsap.registerPlugin(ExpoScaleEase);
 
-  const home = useRef();
-  // const box = useRef();
-  const heading = useRef()
+  const home = useRef(null);  
 
-  useLayoutEffect(() =>{
+  useEffect(() =>{
+    
     let ctx = gsap.context(() => {
 
-    gsap.fromTo(".heading", { autoAlpha:0, x: 150 , duration: 2},{ autoAlpha: 1, x:0 })
-    gsap.fromTo(".home-images",{ delay:3,stagger: 1,scale:0},{ duration: 2.5, autoAlpha: 1 , scale:1, ease: "expoScale( 0.01, 1 ,Power2.inOut)"});
+    gsap.fromTo(".heading", { autoAlpha:0.01, x: 150 , duration:3},{ autoAlpha: 1, x:0 })
+    gsap.fromTo(".home-images",{ autoAlpha:0.01, delay:1,stagger: 1,scale:0, immediateRender: false},{ duration: 2, autoAlpha: 1 , scale:1, ease: "expoScale( 0.01, 1 , power2.inOut)"});
     }, home);
 
     return() => ctx.revert();
   },[]);
 
 
-  
-
   return (
-    <div className='homepage'>
-    <div className='headings' ref={home}>
-    <h1 className='heading' ref={heading}>XYZ</h1>
-    <h1 className='heading photography-text' ref={heading}>PHOTOGRAPHY</h1>
-    {/* <div className='home-images'> */}
-      <img src= "https://res.cloudinary.com/mosesjayz/image/upload/v1687295658/xyz-%20photography/man_c4rfhr_1_sjehzs.jpg" className='man-image home-images' alt= " a man standing" />
-      <img src="https://res.cloudinary.com/mosesjayz/image/upload/v1687295668/xyz-%20photography/cow_sbdann_1_kdaips.jpg" className='cow-image home-images' alt = " a cow standing" />
-    {/* </div> */}
-    {/* <div className='box' ref={box}></div> */}
+    <div className='homepage' ref={home}>
+    <div className='headings'>
+    <h1 className='heading'>XYZ</h1>
+    <h1 className='heading photography-text' >PHOTOGRAPHY</h1>
+     <div className='home-images'> 
+      <img src= "https://res.cloudinary.com/mosesjayz/image/upload/v1687295658/xyz-%20photography/man_c4rfhr_1_sjehzs.jpg" className='man-image' alt= " a man standing" />
+      </div>
+      <div className='home-images'>
+      <img src="https://res.cloudinary.com/mosesjayz/image/upload/v1687295668/xyz-%20photography/cow_sbdann_1_kdaips.jpg" className='cow-image' alt = " a cow standing" />
+     </div>
     </div>
     </div>
   )
